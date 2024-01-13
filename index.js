1b693c2403d8a203333092d3598412b13462f6a6
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-01-13 19:37:02 +0500

    All Done

---
+++ index.js
@@ -0,0 +1,68 @@
+    // 1-MASALA
+// let arr = [`A`, `B`];
+// let [b, a] = arr;
+
+
+
+    // 3-MASALA
+// let arr = [1, 2, 3, 4, 5];
+// let [a, b, ...res] = arr;
+// let new_array = [];
+// new_array.push(a, b)
+
+// console.log(new_array);
+// console.log(res);
+
+
+
+
+    // 4-MASALA
+// let person = {
+//     name: 'John',
+//     age: 30
+// };
+
+// let {name, age} = person;
+// console.log(name);
+// console.log(age);
+
+
+
+
+    // 5-MASALA
+// let car = {
+//     name: 'Mersedes',
+//     year: 2023,
+//     color: 'black'
+// };
+
+// let {name = 'bmw', year = 2020, color = 'white', max_speed= '300km/h'} = car;
+// console.log(name, year, color, max_speed);
+
+
+
+
+    // 6-MASALA
+// let users = [
+//     {
+//         id: 1,
+//         name: 'John',
+//         age: 21
+//     },
+
+//     {
+//         id: 2,
+//         name: `Jack`,
+//         age: 34
+//     },
+
+//     {
+//         id: 3,
+//         name: 'Michael',
+//         age: 43
+//     }
+// ];
+
+// let {id, name, age} = users[0];
+// let {id, name, age} = users[1];
+// let {id, name, age} = users[2];
